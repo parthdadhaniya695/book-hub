e8ff0d40896ff3904f6587af357de08d90d45489
dadhaniyaparth <parthdadhaniya6@gmail.com>
2025-08-13 08:54:34 +0530

    2 table added

---
+++ prisma/migrations/20250813025004_added/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE `book_categories` (
+    `category_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `category_name` VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (`category_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
